89461e4c637c7d1ad0949c4eb4a099e923db2302
jwoon <asasbo21@gmail.com>
2023-11-10 12:29:36 +0900

    @app 추가

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, request, Response
 from collections import Counter
 import json
 
@@ -13,4 +13,17 @@ def hello_world():
     counter = dict(Counter(user_string))
     result = json.dumps(counter)
     return result
+
+@app.get('/count/')
+def count():
+    return render_template('count.html')
+
+@app.post('/result/')
+def result():
+    user_input = request.form['userinput']
+    word_dict = dict(Counter(user_input.split()))
+    result = json.dumps(word_dict)
+    return Response(result,
+                    mimetype='application/json',
+                    headers={'Content-Disposition': 'attachment;filename=count.json'})
 
